a133e03c572ec92d9ebe9e0e3d6ee8bdc8b7fdb6
6W3N <mukaiminew@gmail.com>
2021-10-10 20:57:16 +0900

    add templete

---
+++ R/thesis.R
@@ -0,0 +1,17 @@
+#' @title R Markdown output formats Template
+#' @importFrom rmarkdown render pdf_document
+#' @param Rmd_file file name of R Markdown file
+#' @param Bib_file file name of Bib file
+#' @export
+tsukuba_thesis  <- function(Rmd_file, Bib_file) {
+  template_tex_file <- system.file("rmarkdown/templates/thesis_tsukubabp/resources/templete.tex",
+                                   package = 'TsukubaRmdBP')
+  format_pdf <- rmarkdown::pdf_document(latex_engine = "xelatex",
+                                        template = template_tex_file,
+                                        keep_tex = TRUE,
+                                        toc = TRUE,
+                                        toc_depth = 3,
+                                        highlight = 'tango')
+  format_pdf$inherits <- "pdf_document"
+  format_pdf
+}
